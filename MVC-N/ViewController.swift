d920d848ffa3a9ae523de799d46b892ab9d8e2db
IvanyukovI <98633968+IvanyukovI@users.noreply.github.com>
2022-02-15 22:15:10 +0700

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Игорь Иванюков on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
